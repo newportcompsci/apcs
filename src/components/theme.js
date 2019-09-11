2891a29e892e2ec488b58dba4a919017b8b5f2f3
Josh Rasmussen <joshrasmussen34@gmail.com>
2019-09-11 09:57:57 -0700

    Initial Commit

---
+++ src/components/theme.js
@@ -0,0 +1,17 @@
+export const googleFont =
+  "https://fonts.googleapis.com/css?family=Roboto|Open+Sans|Press+Start+2P"
+
+export const theme = {
+  colors: {
+    black: `#505050`,
+    white: `#eeeeee`,
+    primary: `#E15554`,
+    secondary: `#5e1d1d`,
+    text: `#505050`,
+    background: `#eeeeee`,
+  },
+  fonts: {
+    heading: `"Press Start 2P", sans-serif`,
+    body: `"Open Sans", sans-serif`,
+  },
+}
